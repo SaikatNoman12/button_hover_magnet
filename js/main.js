ad619dceeff731553c543778ed003980ba8c9e5f
saikatnoman12 <noommaa240@gmail.com>
2022-10-18 14:12:41 +0600

    add js button hover magnet

--- js/main.js
+++ js/main.js
@@ -1,8 +1,45 @@
-const btn = document.querySelector('.button');
+const button = document.querySelectorAll('.button');
 const pointer = document.querySelector('.pointer');
 
 
+
+
+
+button.forEach(buttonBox => {
+
+    buttonBox.addEventListener('mousemove', (e) => {
+
+        const position = buttonBox.getBoundingClientRect();
+
+        console.log(position.width);
+
+        const x = e.pageX - position.left - position.width / 2;
+        const y = e.pageY - position.top - position.height / 2;
+
+        buttonBox.style.transform = `translate(${x * .15}px, ${y * .15}px)`;
+
+        buttonBox.children[0].style.transform = `translate(${x * .2}px, ${y * .2}px)`;
+
+        pointer.classList.add('buttonHover');
+
+    });
+
+    buttonBox.addEventListener('mouseout', () => {
+
+        buttonBox.style.transform = `translate(0px, 0px)`;
+        buttonBox.children[0].style.transform = `translate(0px, 0px)`;
+
+        pointer.classList.remove('buttonHover');
+
+    });
+
+});
+
+
+
+
+// pointer event
 window.addEventListener('mousemove', (e) => {
     pointer.style.left = e.clientX + 'px';
-    pointer.style.top = e.clientY + 'px';
-});
+    pointer.style.top = e.clientY + 'px'
+})
